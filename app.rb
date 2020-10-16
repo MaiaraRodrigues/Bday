08676d3bc6f3267e0f906f57e7ebfd0d62f2c9ed
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-16 11:49:44 +0100

    Can view how many days untill next birthday

--- app.rb
+++ app.rb
@@ -1,10 +1,18 @@
 require 'sinatra'
 require 'shotgun'
+require 'date'
 
 class Bday < Sinatra::Base
+  set :session_secret, 'super secret'
 
   get '/' do
     erb(:index)
   end
 
+  post '/birthday' do
+    @birthday = Date.parse(params[:user_birthday])
+    @bday = Date.new(Date.today.year, @birthday.month, @birthday.day)
+    @bday += 365.25 if Date.today >= @bday 
+    "Your birthday is in #{(@bday - Date.today).to_i} days"
+  end
 end 
--- spec/features/inputs_spec.rb
+++ spec/features/inputs_spec.rb
@@ -7,7 +7,7 @@ feature 'Home page' do
   scenario 'User fills in the forms and submits' do
     visit('/')
     fill_in "Name",	with: "Beca"
-    fill_in "Birthday", with: "02/03/1989"
+    fill_in "user_birthday", with: "02/03/1989"
     click_button('Submit')
     expect(page).to have_content ('Your birthday is in 137 days') 
   end
