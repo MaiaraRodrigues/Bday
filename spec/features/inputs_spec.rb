11b19cd0042ae665aa20d2526c306c2ad20eb0f2
Kiril <kiril@olgas-air.mynet>
2020-10-16 10:50:01 +0100

    test for filling in the forms and going to next page + form

--- spec/features/inputs_spec.rb
+++ spec/features/inputs_spec.rb
@@ -2,5 +2,13 @@ feature 'Home page' do
   scenario 'User enters homepage' do
     visit('/')
     expect(page).to have_content ('Hey, you!')
-  end 
+  end
+
+  scenario 'User fills in the forms and submits' do
+    visit('/')
+    fill_in "Name",	with: "Beca"
+    fill_in "Birthday", with: "02/03/1989"
+    click_button('Submit')
+    expect(page).to have_content ('Your birthday is in 137 days') 
+  end
 end
