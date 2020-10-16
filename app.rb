61801c075a0221dfd36371cc7f71efcd6bd6850e
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-16 12:47:03 +0100

    added birthday views

--- app.rb
+++ app.rb
@@ -12,11 +12,6 @@ class Bday < Sinatra::Base
   post '/birthday' do
     @birthday = Date.parse(params[:user_birthday])
     @bday = Date.new(Date.today.year, @birthday.month, @birthday.day)
-    if @bday == Date.today 
-      "Happy Birthday you lil' puta, ciao bella"
-    else
-      @bday += 365.25 if Date.today >= @bday 
-      "Your birthday is in #{(@bday - Date.today).to_i} days"
-    end
+      erb :birthday
   end
 end 
